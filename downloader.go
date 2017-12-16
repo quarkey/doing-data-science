35f77fb70f9bee68dd4369d805bf9aa4a484892d
Steffen Lundin <steffen.sandvold@gmail.com>
2017-12-16 15:45:43 +0100

    cleaning up

---
+++ downloader.go
@@ -0,0 +1,102 @@
+/*
+
+The purpose of this program is to download all statistical data from a given URL and process
+the data in somewhat useful manner. It is an exercise taken from O'Reilly - Doing Data Science at page 37.
+
+Exercice: EDA - Exploratory Data Analysis
+
+There are 31 datasets named nyt1.csv, nyt2.csv and so on, which you can find here:
+https://github.com/oreillymedia/doing_data_sience
+
+1. age_groups at <18, 18-24, 25-34, 35-44, 45-54, 56-64 and 65+
+
+2. plot the distributions of number impression and
+
+3. extend your analysis cross days. visualize some metrics and distributions over time.
+
+4. describe and interpet any patterns you find
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+const (
+	downloadpath = "download"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	baseurl := flag.String("baseurl", "", "baseurl")
+	sprintf := flag.String("sprintf", "", "spritnf")
+	ntimes := flag.Int("n", 0, "n times")
+	flag.Parse()
+
+	if *baseurl == "" {
+		fmt.Println("Missing baseurl!")
+		os.Exit(1)
+	}
+	if *sprintf == "" {
+		fmt.Println("Missing sprintf!")
+		os.Exit(1)
+	}
+	if *ntimes == 0 {
+		fmt.Println("Missing ntimes!")
+		os.Exit(1)
+	}
+
+	urls := buildURLs(*baseurl, *sprintf, *ntimes)
+	// urls := buildURLs("http://stat.columbia.edu/~rachel/datasets/", "nyt%d.csv", *ntimes)
+	if err := download(urls, downloadpath); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// BuildURLs is creating urls
+func buildURLs(url, sprintf string, ntimes int) (urls []string) {
+	for i := 1; i < ntimes+1; i++ {
+		url := url + fmt.Sprintf(sprintf, i)
+		// fmt.Println("builing url:", url)
+		urls = append(urls, url)
+	}
+	return urls
+}
+
+// Download file from urls and save output to destitation folder concurrently
+func download(urls []string, dest string) error {
+	_, err := os.Stat(dest)
+	if err != nil {
+		log.Fatalf("cannot stat desination folder: %v\n", err)
+	}
+	for _, url := range urls {
+		wg.Add(1)
+		go fetch(url, dest)
+	}
+	wg.Wait()
+	return nil
+}
+func fetch(url, dest string) {
+	defer wg.Done()
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	fmt.Printf("downloading file %s\n", url)
+	out, err := os.Create(dest + "/" + filepath.Base(url))
+	if err != nil {
+		fmt.Printf("Unable to create file: %v\n", err)
+	}
+	defer out.Close()
+	io.Copy(out, resp.Body)
+	fmt.Printf("%s done!\n", filepath.Base(url))
+}
