e3bf952537133644ee435caf2cce3063f092070d
Steffen Lundin <steffen.sandvold@gmail.com>
2017-12-16 16:14:25 +0100

    moving exercice text to readme

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ oreilly doing data science
 
 ## concurrent downloader
 example: ./downloader --baseurl=http://stat.columbia.edu/~rachel/datasets/ --sprintf="nyt%d.csv" --n 31
+
+
+Exercice: EDA - Exploratory Data Analysis
+
+There are 31 datasets named nyt1.csv, nyt2.csv and so on, which you can find here:
+https://github.com/oreillymedia/doing_data_sience
+
+1. age_groups at <18, 18-24, 25-34, 35-44, 45-54, 56-64 and 65+
+
+2. plot the distributions of number impression and
+
+3. extend your analysis cross days. visualize some metrics and distributions over time.
+
+4. describe and interpet any patterns you find
--- downloader.go
+++ downloader.go
@@ -1,21 +1,8 @@
 /*
-
 The purpose of this program is to download all statistical data from a given URL and process
 the data in somewhat useful manner. It is an exercise taken from O'Reilly - Doing Data Science at page 37.
-
-Exercice: EDA - Exploratory Data Analysis
-
-There are 31 datasets named nyt1.csv, nyt2.csv and so on, which you can find here:
-https://github.com/oreillymedia/doing_data_sience
-
-1. age_groups at <18, 18-24, 25-34, 35-44, 45-54, 56-64 and 65+
-
-2. plot the distributions of number impression and
-
-3. extend your analysis cross days. visualize some metrics and distributions over time.
-
-4. describe and interpet any patterns you find
 */
+
 package main
 
 import (
